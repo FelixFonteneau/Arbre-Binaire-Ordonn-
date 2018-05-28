9d870c66e8bc28f3ea9c8249df37ccb8a9f8a502
savper <35189240+savper@users.noreply.github.com>
2018-05-28 13:17:33 +0200

    Add files via upload

--- NoeudDG.java
+++ NoeudDG.java
@@ -1,48 +1,21 @@
 import PaD.*;
 
 public class NoeudDG{
+
+	private final static PlancheADessin pad = new PlancheADessin();
+
+
 	double milieu_x = pad.getLargeur()/2;
 	double milieu_y = pad.getHauteur()/2;
 	double a1,a2,b1,b2;
 	
-	public void squeletteArbre (int n){ //n est le nombre de niveaux max
-		int i=0;
-
-		for (int i=1, i<=n){
-			a1= pad.getLargeur()/2**i;
-			a2= pad.getHauteur()/2**i;
-		}
-
-		
-
-		
+	 n(x,y,rayon,PlancheADessin.VERT);
+		pad.ajouter(BrancheD,BrancheG,NoeudOrig);
 	}	
-	
-
 
 
 	public static void main(String [] args){
-		PlancheADessin pad = new PlancheADessin();
-		double milieu_x = pad.getLargeur()/2;
-		double milieu_y = pad.getHauteur()/2;
+		Noeud_Branches(0,pad.getLargeur()/2,pad.getHauteur()/2);
 		
-		double a1= pad.getLargeur()/4;
-		double a2= pad.getHauteur()/4;
-
-		double b1= pad.getLargeur()/8;
-		double b2= pad.getLargeur()/8;
-
-		double c = 20; //diametre des noeuds
-
-		Dessinable BrancheD = new Ligne(milieu_x,c,milieu_x+a1,c+a2,PlancheADessin.NOIR,3);
-		Dessinable BrancheG = new Ligne(milieu_x,c,milieu_x-a1,c+a2,PlancheADessin.NOIR,3);
-
-		Dessinable NoeudOrig = new CerclePlein(milieu_x,c,c,PlancheADessin.VERT);
-
-		Dessinable NoeudD = new CerclePlein(milieu_x+a1,c+a2,c,PlancheADessin.ROUGE);
-		Dessinable NoeudG = new CerclePlein(milieu_x-a1,c+a2,c,PlancheADessin.ROUGE);
-		
-
-		pad.ajouter(NoeudOrig,BrancheD,BrancheG,NoeudD,NoeudG);
 	}
 }
