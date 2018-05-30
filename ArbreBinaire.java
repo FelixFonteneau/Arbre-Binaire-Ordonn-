ba07ca91629767bdaf7c2392dcf064b7c06643ba
savper <35189240+savper@users.noreply.github.com>
2018-05-30 20:42:07 +0200

    Update ArbreBinaire.java

--- ArbreBinaire.java
+++ ArbreBinaire.java
@@ -1,7 +1,7 @@
 /*
-  * ArbreBinaire est l'interface, donc elle définie les méthodes employées.
+  * ArbreBinaire est l'interface, donc elle définit les méthodes employées.
   *
-  * La classe abstraite ArbreBinaireAbstract nous sert de définir "l'interface graphique"
+  * La classe abstraite ArbreBinaireAbstract nous sert à définir "l'interface graphique".
 */
 
 public interface ArbreBinaire<C extends Comparable<C>, V> {
